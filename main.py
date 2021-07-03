11ac1a88f4626baac0758d9dd6f5d4be062429fe
Ryan Salvanera <ryan.salvanera@gmail.com>
2021-07-03 18:28:27 +0800

    initial and final commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+scoreboard = Scoreboard()
+
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        scoreboard.increase_level()
+        car_manager.level_up()
+
+screen.exitonclick()
---
+++ player.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.setheading(90)
+        self.reset_position()
+
+    def reset_position(self):
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.ycor() >= FINISH_LINE_Y:
+            self.reset_position()
+            return True
+        return False
